d7e918d60fc96a72627d43e5409c651250721bf7
TheEpicBlock <git.teb@theepicblock.nl>
2021-10-16 23:09:04 +0200

    Remove fly checks

---
+++ src/main/java/nl/theepicblock/notflying/mixin/DisableFlyCheck.java
@@ -0,0 +1,23 @@
+package nl.theepicblock.notflying.mixin;
+
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class DisableFlyCheck {
+    @Shadow private boolean floating;
+
+    @Shadow private int floatingTicks;
+
+    @Shadow private boolean vehicleFloating;
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void setNotFlying(CallbackInfo ci) {
+        this.floating = false;
+        this.vehicleFloating = false;
+    }
+}
